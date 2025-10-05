1bc9bae70cd40f2bedea8880cd37affda3c6a833
Beknur <beknurserikkaly@gmail.com>
2025-10-06 04:49:28 +0500

    init: maven project structure, junit5, ci setup

---
+++ assignment2-insertion-sort/src/main/java/com/beknur/App.java
@@ -0,0 +1,13 @@
+package com.beknur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
